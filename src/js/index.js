6918edd81b9078a0373f6932143311251debc2e1
AkiaWilliams <59836109+AkiaWilliams@users.noreply.github.com>
2020-03-12 10:30:45 -0400

    done

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-import "file-loader?name=[name].[ext]!../index.html";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
-import "../style/index.scss";
-
-window.onload = function() {
-  console.log("Everything is working!");
-};
